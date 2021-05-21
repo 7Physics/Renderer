13646698a4e0877a7ea6c156f75e588c80df92aa
TURC Eléa <eleaturc@outlook.fr>
2021-05-21 21:37:50 +0200

    else if to switch case

--- src/main/java/fr/setphysics/renderer/WorldRenderer.java
+++ src/main/java/fr/setphysics/renderer/WorldRenderer.java
@@ -24,39 +24,38 @@ public class WorldRenderer implements GLEventListener, KeyListener {
 	@Override
 	public void display(GLAutoDrawable drawable) {
 		final GL2 gl = drawable.getGL().getGL2();
-        gl.glLoadIdentity();
-        gl.glTranslatef(transX, transY, zoom-5.0f);
-        gl.glRotatef(rotateX, 1.0f, 0.0f, 0.0f);
-        gl.glRotatef(rotateY, 0.0f, 1.0f, 0.0f);
-        gl.glRotatef(rotateZ, 0.0f, 0.0f, 1.0f);
+		gl.glLoadIdentity();
+		gl.glTranslatef(transX, transY, zoom - 5.0f);
+		gl.glRotatef(rotateX, 1.0f, 0.0f, 0.0f);
+		gl.glRotatef(rotateY, 0.0f, 1.0f, 0.0f);
+		gl.glRotatef(rotateZ, 0.0f, 0.0f, 1.0f);
 
-        
-        gl.glEnable(GL2.GL_BLEND);
-        gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
-        gl.glClearColor(0.195f, 0.590f, 0.656f, 1.0f);
+		gl.glEnable(GL2.GL_BLEND);
+		gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
+		gl.glClearColor(0.195f, 0.590f, 0.656f, 1.0f);
 
-        gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+		gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
 
-        gl.glBegin(GL2.GL_QUADS);
-        
-        gl.glColor4f(0.3f, 0.3f, 0.3f, 0.8f);
-        gl.glVertex3f(2.0f, 0f, 2.0f);
-        gl.glVertex3f(2.0f, 0f, -2.0f);
-        gl.glVertex3f(-2.0f, 0f, -2.0f);
-        gl.glVertex3f(-2.0f, 0f, 2.0f);
-        gl.glEnd();
+		gl.glBegin(GL2.GL_QUADS);
 
-        gl.glBegin(GL2.GL_LINES);
-        gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-        for(int i = 0; i <= 20; i++) {
-            gl.glVertex3f((i/5f)-2f, 0f, 2f);
-            gl.glVertex3f((i/5f)-2f, 0f, -2f);
+		gl.glColor4f(0.3f, 0.3f, 0.3f, 0.8f);
+		gl.glVertex3f(2.0f, 0f, 2.0f);
+		gl.glVertex3f(2.0f, 0f, -2.0f);
+		gl.glVertex3f(-2.0f, 0f, -2.0f);
+		gl.glVertex3f(-2.0f, 0f, 2.0f);
+		gl.glEnd();
 
-            gl.glVertex3f(2f, 0f, (i/5f)-2f);
-            gl.glVertex3f(-2f, 0f, (i/5f)-2f);
-        }
-        gl.glEnd();
-        
+		gl.glBegin(GL2.GL_LINES);
+		gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+		for (int i = 0; i <= 20; i++) {
+			gl.glVertex3f((i / 5f) - 2f, 0f, 2f);
+			gl.glVertex3f((i / 5f) - 2f, 0f, -2f);
+
+			gl.glVertex3f(2f, 0f, (i / 5f) - 2f);
+			gl.glVertex3f(-2f, 0f, (i / 5f) - 2f);
+		}
+		gl.glEnd();
+
 	}
 
 	@Override
@@ -78,30 +77,43 @@ public class WorldRenderer implements GLEventListener, KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if (e.getKeyChar() == 'a') {
+		switch (e.getKeyChar()) {
+		case 'a':
 			zoom += 0.1f;
-		} else if (e.getKeyChar() == 'e') {
+			break;
+		case 'e':
 			zoom -= 0.1f;
-		} else if (e.getKeyChar() == 'd') {
+			break;
+		case 'd':
 			transX += 0.1f;
-		} else if (e.getKeyChar() == 'q') {
+			break;
+		case 'q':
 			transX -= 0.1f;
-		} else if (e.getKeyChar() == 's') {
+			break;
+		case 's':
 			transY += 0.1f;
-		} else if (e.getKeyChar() == 'z') {
+			break;
+		case 'z':
 			transY -= 0.1f;
-		} else if (e.getKeyChar() == 'o') {
+			break;
+		case 'o':
 			rotateX += 1.0f;
-		} else if (e.getKeyChar() == 'p') {
+			break;
+		case 'p':
 			rotateX -= 1.0f;
-		} else if (e.getKeyChar() == 'l') {
+			break;
+		case 'l':
 			rotateY += 1.0f;
-		} else if (e.getKeyChar() == 'm') {
+			break;
+		case 'm':
 			rotateY -= 1.0f;
-		} else if (e.getKeyChar() == ';') {
+			break;
+		case ';':
 			rotateZ += 1.0f;
-		} else if (e.getKeyChar() == ':') {
+			break;
+		case ':':
 			rotateZ -= 1.0f;
+			break;
 		}
 
 	}
